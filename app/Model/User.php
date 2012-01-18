5dd6ae73c5e5525836dd62560ffdc4f6e447f0b2
Parker Moore <parkrmoore@gmail.com>
2012-01-18 00:48:47 -0500

    got rid of requiring the 'id' field in User

--- app/Model/User.php
+++ app/Model/User.php
@@ -37,14 +37,6 @@ class User extends AppModel {
 				//'last' => false, // Stop validation after this rule
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
 		),
 		'role' => array(
 			'alphanumeric' => array(
