531c6b3241752d50f5ba20849b4b0c3b539b8db5
Parker Moore <parkrmoore@gmail.com>
2012-02-10 01:59:22 -0500

    Separates the program into sub-pages based on day. Fixes #42.

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -9,7 +9,7 @@ class EventsController extends AppController {
 
 	public function beforeFilter() {
 		parent::beforeFilter();
-		$this->Auth->allow('view');
+		$this->Auth->allow('view', 'thursday', 'friday', 'saturday');
 	}
 	
 /**
@@ -19,9 +19,26 @@ class EventsController extends AppController {
  */
 	public function index() {
 		$this->set('prevpage_for_layout', array('title' => "Home", 'routing' => '/'));
-		$this->set('thursday', $this->Event->find('all', $this->_getIndexCondsOnDate(2012, 04, 12)));
-		$this->set('friday', $this->Event->find('all', $this->_getIndexCondsOnDate(2012, 04, 13)));
-		$this->set('saturday', $this->Event->find('all', $this->_getIndexCondsOnDate(2012, 04, 14)));
+		$this->set('days', array(
+			'Thursday, April 12, 2012' => array('controller' => 'events', 'action' => 'thursday'),
+			'Friday, April 13, 2012' => array('controller' => 'events', 'action' => 'friday'),
+			'Saturday, April 14, 2012' => array('controller' => 'events', 'action' => 'saturday'),
+		));
+	}
+	public function thursday(){
+		$this->set('title_for_layout', $event['Event']['name']);
+		$this->set('today', $this->Event->find('all', $this->_getIndexCondsOnDate(2012, 04, 12)));
+		$this->set('prevpage_for_layout', array('title' => "Program", 'routing' => array('controller' => 'events', 'action' => 'index')));
+	}
+	public function friday(){
+		$this->set('title_for_layout', $event['Event']['name']);
+		$this->set('today', $this->Event->find('all', $this->_getIndexCondsOnDate(2012, 04, 13)));
+		$this->set('prevpage_for_layout', array('title' => "Program", 'routing' => array('controller' => 'events', 'action' => 'index')));
+	}
+	public function saturday(){
+		$this->set('title_for_layout', $event['Event']['name']);
+		$this->set('today', $this->Event->find('all', $this->_getIndexCondsOnDate(2012, 04, 14)));
+		$this->set('prevpage_for_layout', array('title' => "Program", 'routing' => array('controller' => 'events', 'action' => 'index')));
 	}
 	
 	private function _getIndexCondsOnDate($year, $month, $day){
@@ -51,6 +68,9 @@ class EventsController extends AppController {
 		$event = $this->Event->read(null, $id);
 		$this->set('event', $event);
 		$this->set('title_for_layout', $event['Event']['name']);
-		$this->set('prevpage_for_layout', array('title' => ucwords($this->params['controller']), 'routing' => array('controller' => $this->params['controller'], 'action' => 'index')));
+		$this->set('prevpage_for_layout', array(
+			'title' => ucwords(date('l', strtotime($event['Event']['start_time']))), 
+			'routing' => array('controller' => $this->params['controller'], 'action' => strtolower(date('l', strtotime($event['Event']['start_time']))))
+		)); // Reroute to the day from whence you came
 	}
 }
--- app/View/Events/index.ctp
+++ app/View/Events/index.ctp
@@ -1,42 +1,7 @@
-<?php
-	
-$days = array(
-	array(
-		"date" => "Thursday, April 12, 2012",
-		"events" => $thursday
-	),
-	array(
-		"date" => "Friday, April 13, 2012",
-		"events" => $friday
-	),
-	array(
-		"date" => "Saturday, April 14, 2012",
-		"events" => $saturday
-	),
-);
-	
-?>
-<ul data-role="listview" data-filter="true" data-theme="a">
-<?php foreach($days as $day): ?>
-	<li data-role="list-divider" data-theme="c"><?php echo $day['date']; ?></li>
-	<?php foreach ($day['events'] as $event): ?>
+<ul data-role="listview" data-inset="true" data-theme="a">
+<?php foreach($days as $day => $routing): ?>
 	<li data-theme="a">
-		<a href="<?php echo $this->Html->url(array('action' => 'view', $event['Event']['id'])); ?>">
-			<h3><?php echo h($event['Event']['name']); ?>&nbsp;</h3>
-			<p>
-				<?php echo h($event['Event']['description']); ?>&nbsp;
-			</p>
-			<p class="ui-li-aside">
-				<strong>
-					<?php echo h(date("g:i a", strtotime($event['Event']['start_time']))); ?> 
-					&mdash; 
-					<?php echo h(date("g:i a", strtotime($event['Event']['end_time']))); ?>
-				</strong>
-				in 
-				<strong><?php echo h($event['Event']['location']); ?></strong>
-			</p>
-		</a>
+		<?php echo $this->Html->link($day, $routing); ?>
 	</li>
-	<?php endforeach; ?>
 <?php endforeach;?>
 </ul>
