a65a3dc41659dfc771432020a679a2ddb5bae51b
Parker Moore <parkrmoore@gmail.com>
2012-02-10 15:40:41 -0500

    No longer delaying CSS changes on page load.

--- app/webroot/js/application.js
+++ app/webroot/js/application.js
@@ -1,13 +1,11 @@
 $(document).live('pageinit', function(event) {
-	$.mobile.ajaxLinksEnabled = false;
     $.extend($.mobile, {
 		ajaxEnabled: false,
 		ajaxLinksEnabled: false,
 		ajaxFormsEnabled: false
 	});
-	console && console.log && console.log("Changing .ui-bar-c to .ui-bar-a");
-	setTimeout(function(){ 
+	(function(){
 		$('[role="search"]').removeClass("ui-bar-c");//.addClass("ui-bar-a")
 		$('[data-theme="c"]').removeClass("ui-bar-b").addClass("ui-bar-c");
-	}, 100);	
+	})();
 });
