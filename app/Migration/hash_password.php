576d113f5bb0da07465acc9e68bf4b9ede45c50e
Parker Moore <parkrmoore@gmail.com>
2012-04-10 14:39:00 -0400

    Removed hash_password CLI utility in favour of something similar on the admin site.

--- app/Migration/hash_password.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$password = ($argv[0] == basename(__FILE__)) ? $argv[1] : $argv[0];
-require_once(dirname(__FILE__)."/../../index.php");
-$hashed = AuthComponent::password($password);
-echo $hashed."\n";
-?>
